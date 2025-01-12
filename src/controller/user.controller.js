384f085708f4f1361545d0309b56fa5ef28f6b36
gaurav060404 <gs762282@gmail.com>
2025-01-12 19:31:29 +0530

    Fix refresh token variable name in access token refresh logic

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -194,19 +194,19 @@ const refreshAcessToken = asyncHandler(async (req, res) => {
       secure: true,
     };
 
-    const { accessToken, newRefreshToken } =
+    const { accessToken, refreshToken } =
       await generateAccessTokenandRefreshToken(user._id);
 
     return res
       .status(200)
       .cookie("accessToken", accessToken, options)
-      .cookie("refreshToken", newRefreshToken, options)
+      .cookie("refreshToken", refreshToken, options)
       .json(
         new ApiResponse(
           200,
           {
             accessToken,
-            refreshToken: newRefreshToken,
+            refreshToken: refreshToken,
           },
           "Access token refreshed successfully"
         )
